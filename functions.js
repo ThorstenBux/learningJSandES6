0a3147f5bb735c6d9af08d920f44fea2765d7bdb
Thorsten Bux <thorsten.bux_c@daqri.com>
2017-08-01 18:14:00 +1200

    add documentation

--- functions.js
+++ functions.js
@@ -1,5 +1,7 @@
 //(1) using named functions 
 //(2) passing the named function directly as parameter
+//(3) pass an anonymous function directly as parameter
+//(4) use ES6 syntax with anonymous function
 
 // function declaration that takes in two arguments: a function for displaying
 // a message, along with a name of a movie
@@ -16,17 +18,16 @@ const favoriteMovie = function displayFavorite(movieName){
 movies(favoriteMovie,name);
 
 
-
 //(2) call the movies function, pass in the named-function and name of movie
 movies(function displayFavorite2(movieName) {
   console.log("My favorite movie is " + movieName);
 }, "Finding Nemo");
 
 
-// call the movies function, pass in the anonymous-function and name of movie
+//(3) call the movies function, pass in the anonymous-function and name of movie
 movies(function (movieName) {
   console.log("My favorite movie is " + movieName);
 }, "Finding Nemo");
 
-// call the movies function, pass in the function and name of movie ES6 syntax
+//(4) call the movies function, pass in the function and name of movie ES6 syntax
 movies(movieName => console.log("My favorite movie is " + movieName), "Finding Nemo");
