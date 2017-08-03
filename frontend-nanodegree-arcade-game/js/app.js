61379c7e7460d28d752b1fbd06d2e8dedec1101a
Thorsten Bux <thorsten.bux_c@daqri.com>
2017-08-03 22:50:05 +1200

    added collision

--- frontend-nanodegree-arcade-game/js/app.js
+++ frontend-nanodegree-arcade-game/js/app.js
@@ -1,6 +1,11 @@
+const MAX_X = 400;
+const MAX_Y = 400;
+const ENEMY_MAX_Y = 230;
+const ENEMY_MIN_Y = 20;
+
 //General figure class that is base class for all figures in the game
 class Figure {
-    constructor(sprite, loc, speed = getRandomIntInclusive(10,50)){
+    constructor(sprite, loc, speed = getRandomIntInclusive(10,50),boundingBoxX = 20, boundingBoxY = 10){
         //A string containing the image used to visualize the figure on the board.
         this.sprite = sprite;
 
@@ -9,11 +14,26 @@ class Figure {
 
         //The figure speed which is a random value either 10 or 50 per default or passed in the constructor
         this.speed = speed;
+
+        this.boundingBoxXValue = boundingBoxX;
+        this.boundingBoxYValue = boundingBoxY;
+
+        //The bounding box surrounding the figures
+        this.boundingBox = {};
+        this.updateBoundingBox();
     }
     // Draw the figure on the screen, required method for game
     render(){
         // @ts-ignore
         ctx.drawImage(Resources.get(this.sprite), this.loc.x, this.loc.y);
+    }
+    updateBoundingBox(){
+        //We calculate a bounding box around the figure
+        this.boundingBox.xLeft = this.loc.x - this.boundingBoxXValue;
+        this.boundingBox.xRight = this.loc.x + this.boundingBoxXValue;
+        this.boundingBox.yDown = this.loc.y + this.boundingBoxYValue;
+        this.boundingBox.yUp = this.loc.y - this.boundingBoxYValue;
+        //console.log(`BoundingBox: ${this.boundingBox.xRight} ${this.boundingBox.xLeft} ${this.boundingBox.yUp} ${this.boundingBox.yDown}`);
     }
 }
 
@@ -26,8 +46,8 @@ class Enemy extends Figure{
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     constructor(sprite = 'images/enemy-bug.png'){
-        super(sprite,{x:0,y:getRandomIntInclusive(2,4)});
-        console.log(`Speed: ${this.speed} Loc: ${this.loc.x} ${this.loc.y}`);
+        super(sprite,{x:0,y:getRandomIntInclusive(ENEMY_MIN_Y,ENEMY_MAX_Y)});
+        console.log(`Enemy: Speed: ${this.speed} Loc: ${this.loc.x} ${this.loc.y} BoundingBox: xRight:${this.boundingBox.xRight} xLeft:${this.boundingBox.xLeft} xUp:${this.boundingBox.yUp} xDown:${this.boundingBox.yDown}`);
     }
     // Update the enemy's position, required method for game
     // Parameter: dt, a time delta between ticks
@@ -35,15 +55,25 @@ class Enemy extends Figure{
         // You should multiply any movement by the dt parameter
         // which will ensure the game runs at the same speed for
         // all computers.
-        this.loc.x = this.loc.x + (this.speed * dt);
+        this.loc.x = this.loc.x + this.speed * dt;
+
+        if(this.loc.x >= MAX_X)
+        {
+            this.loc.x = 0;
+            this.loc.y = getRandomIntInclusive(ENEMY_MIN_Y,ENEMY_MAX_Y);
+        }
+        this.updateBoundingBox();
+        this.checkForCollision();
     }
-    collision(){
-        //TODO implement collision with player
+    checkForCollision(){
+        //Now we check if the boundingBox of Enemy and Player collide
+
+        if( (this.boundingBox.xRight >= player.boundingBox.xLeft && this.boundingBox.yDown >= player.boundingBox.yUp && this.boundingBox.xLeft <= player.boundingBox.xRight) ) {
+                console.log("Collission!!!");
+        }
         return false;
     }
 };
-
-
 
 // Now write your own player class
 // This class requires an update(), render() and
@@ -52,27 +82,43 @@ class Player extends Figure{
     //Load the initial image for the character, if nothing is provided we use char-horn-girl.png
     constructor(sprite='images/char-horn-girl.png'){
         //Load the initial location for the character; y is always 0 x is random between 0-4
-        super(sprite, {x:getRandomIntInclusive(0,4), y:0})
+        super(sprite, {x:getRandomIntInclusive(0,MAX_X), y:MAX_Y},undefined,10,10)
+        console.log(`Player: Speed: ${this.speed} Loc: ${this.loc.x} ${this.loc.y} BoundingBox: xRight:${this.boundingBox.xRight} xLeft:${this.boundingBox.xLeft} xUp:${this.boundingBox.yUp} xDown:${this.boundingBox.yDown}`);
+        this.moveX = 0;
+        this.moveY = 0;
     }
     update(dt){
         // You should multiply any movement by the dt parameter
         // which will ensure the game runs at the same speed for
         // all computers.
-        //this.loc.y = this.loc.y + (this.speed * dt);
+        this.loc.y +=this.moveY;
+        this.loc.x +=this.moveX;
+        this.moveX = 0;
+        this.moveY = 0;
+
+        //Check if we reached the water
+        if(this.loc.y <= 0){
+            console.log("Reached the water, we are save. Restart");
+            resetGame();
+        }
+        this.updateBoundingBox();
     }
     handleInput(key){
+        console.log(key);
+        console.log(`Player: Speed: ${this.speed} Loc: ${this.loc.x} ${this.loc.y} BoundingBox: xRight:${this.boundingBox.xRight} xLeft:${this.boundingBox.xLeft} xUp:${this.boundingBox.yUp} xDown:${this.boundingBox.yDown}`);
+
         switch (key){
             case 'left':
-                this.loc.x--;
+                this.moveX = -this.speed;
                 break;
-            case 'righ':
-                this.loc.x++;
+            case 'right':
+                this.moveX = this.speed
                 break;
             case 'up':
-                this.loc.y++;
+                this.moveY = -this.speed;
                 break;
             case 'down':
-                this.loc.y--;
+                this.moveY = this.speed;
                 break;
         }
     }
@@ -82,9 +128,13 @@ class Player extends Figure{
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 
-const player = new Player();
-const allEnemies = [];
+let player = new Player();
+let allEnemies = [];
 allEnemies.push(new Enemy());
+
+// setTimeout(function(){
+//     allEnemies.push(new Enemy());
+// },1000);
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
@@ -98,6 +148,12 @@ document.addEventListener('keyup', function(e) {
 
     player.handleInput(allowedKeys[e.keyCode]);
 });
+
+function resetGame(){
+    player = new Player();
+    allEnemies = [];
+    allEnemies.push(new Enemy());
+}
 
 function getRandomIntInclusive(min, max) {
   min = Math.ceil(min);
