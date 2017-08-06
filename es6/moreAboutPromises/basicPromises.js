77c57bc55d6e896ddcdbd09e9acea56af3d775b3
Thorsten Bux <thorsten.bux_c@daqri.com>
2017-08-06 18:04:00 +1200

    additional things about promises

--- es6/moreAboutPromises/basicPromises.js
+++ es6/moreAboutPromises/basicPromises.js
@@ -34,3 +34,40 @@ promise.catch(function error(rejectParam){
 
 });
 // --> that is the same as the then() function with two functions as parameters
+//IMPORTANT:
+  // There is a slight difference between .then(successFunc,errorFunc) and .then(successFunc).catch(errorFunc):
+      //In case there is an error inside the successFunc it will be catched in the .catch(errorFunc) it will not be catched in the 
+      //errorFunc passed as 2nd parameter to the .then() function.
+      //This means then().catch() can both be called where as .then(successFunc,errorFunc) only one of them gets called
+// --> That is why it might be better to use .then().catch(). I personally like .then(successFunc,errorFunc) better because when chaining promisses
+// it is easier to spot when the 2nd promises .then() is called
+
+promise.then(success => {
+  console.log("success");
+  //  throw new Exception();  //Attention: In this case, if there is an error in the 1st Promise success function the 2nd-Promise error function is called. (See (*1) for details)
+  return new Promise( () => console.log("2nd Promise"));
+}, error => {
+  console.log("error");
+}).then(success2nd => {       //Here we work with the 2nd Promise returned by the first promise's success function. 
+                              //You have the closing ')' to indicate when the .then function of the first promise is done. The chain looks like this: then().then()
+  console.log("success 2nd");
+},error2nd => { 
+  console.log("error 2nd");
+});
+
+//Using catch that would look like this:
+
+promise.then(success => {
+  console.log("success");
+  //  throw new Exception();  //Attention: In this case, if there is an error in the 1st Promise success function the .catch() function is called. (See (*1) for details)
+  return new Promise( () => console.log("2nd Promise"));
+}).catch( error => {
+  console.log("error");
+}).then(success2nd => {       //Here we work with the 2nd Promise returned by the first promise's success function. 
+                              //You cannot explicitely see when we work with the 2nd promise because then().catch().then().catch() are just joined one after the other.
+  console.log("success 2nd");
+}).catch (error2nd => { 
+  console.log("error 2nd");
+});
+
+//(*1) Attention: As soon as a promise *rejects* the JS-Engine skipps to the next .reject-function in the chain which can be a .catch() or a .then(undefined,rejectFunc) 
